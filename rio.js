5819346d1fb4befad30e0d386c5ed76592fd7aea
Yosh666 <60882174+Yosh666@users.noreply.github.com>
2020-02-11 19:36:38 +0100

    Changement de logo ajout du responsive en CSS burger en JS

---
+++ rio.js
@@ -0,0 +1,7 @@
+document.getElementById("hamburger").onclick= function (){
+    myFunction()
+};
+function myFunction() {
+    document.getElementById("dropdown").classList.toggle ("show")
+    
+};
